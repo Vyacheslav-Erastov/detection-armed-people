1cc46d7f01e82244b0999b54f4192c6d8a742147
Vyacheslav-Erastov <yurkuserastov228@gmail.com>
2023-11-06 20:57:49 +0300

    Work for UI

--- frontend/src/components/Task.tsx
+++ frontend/src/components/Task.tsx
@@ -1,21 +1,31 @@
-import Card from "@mui/material/Card";
+import Stack from "@mui/material/Stack";
 import { ITaskDetailed } from "../types/Task";
-import CardContent from "@mui/material/CardContent";
+import Box from "@mui/material/Box";
+import Chip from "@mui/material/Chip";
 import Typography from "@mui/material/Typography";
 
 interface ITaskDetailedProps {
     task: ITaskDetailed
 }
 
+function CustomStack(props: { key_: string, value: string }) {
+    return (
+        <Stack direction="row" spacing={1} sx={{ margin: "1%" }}>
+            <Typography variant="h6" gutterBottom>
+                {props.key_}:
+            </Typography>
+            <Chip label={props.value} color="primary" />
+        </Stack>
+    )
+}
+
 export function Task({ task }: ITaskDetailedProps) {
     return (
-        <Card sx={{ minWidth: 275 }}>
-            <CardContent>
-                <Typography variant="h5" component="div">
-                    Task Name: {task.name}
-                </Typography>
-            </CardContent>
-        </Card>
+        <Box sx={{ p: 2, border: '1px solid black', width: "20%", margin: "1%", borderRadius: 1 }}>
+            <CustomStack key_="Название задачи" value={task.name} />
+            <CustomStack key_="Тип задачи" value={task.type} />
+            <CustomStack key_="Статус задачи" value={task.status} />
+        </Box>
     );
 }
 
